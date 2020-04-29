bb11ad47366fc05b61f70ce257b2e72d4e34343e
huy <huyan@specialcom.cn>
2020-04-29 14:14:28 +0800

    feat:新协议处理

--- cluster/handler.go
+++ cluster/handler.go
@@ -65,8 +65,10 @@ func cache() {
 	protoMsgJson, err := ioutil.ReadFile("./configs/proto_pomelo.json")
 	if err == nil {
 		log.Println("Register proto json ./configs/proto_msg.json")
+		msgJson := map[string]interface{}{}
+		json.Unmarshal(protoMsgJson, msgJson)
 		sysMap["protos"] = map[string]interface{}{
-			"server": protoMsgJson,
+			"server": msgJson,
 		}
 	}
 
